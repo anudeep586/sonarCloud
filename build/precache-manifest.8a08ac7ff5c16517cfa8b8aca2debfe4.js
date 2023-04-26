5283f367593778589e3128ddb9a98c3581767a4c
L N M Anudeep <61861542+anudeep586@users.noreply.github.com>
2023-04-26 17:43:35 +0530

    Upload precache-manifest.8a08ac7ff5c16517cfa8b8aca2debfe4.js

---
+++ build/precache-manifest.8a08ac7ff5c16517cfa8b8aca2debfe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75479b9fddf0c00baf17f78238117f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "095d0ee739a71aa6bf95",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "dd90ffe06a4c88162ebe",
+    "url": "/static/js/2.7de970da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7de970da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095d0ee739a71aa6bf95",
+    "url": "/static/js/main.87b22cdf.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
