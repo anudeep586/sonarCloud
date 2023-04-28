3dc1a1e7c1a8936b6621d1298505670199707f5a
L N M Anudeep <61861542+anudeep586@users.noreply.github.com>
2023-04-28 20:16:04 +0530

    Upload ExpensesList.js

---
+++ src/Components/Expenses/ExpensesList.js
@@ -0,0 +1,19 @@
+import './ExpensesList.css';
+import ExpenseItem from './ExpenseItem';
+const ExpensesList = props => {
+
+    if(props.items.length === 0){
+        return <h2 className="expense-list__fallback" style={{color: "white"}}>Found no expenses.</h2>
+    }
+    return <ul className="expenses-list">
+        {props.items.map(expense =>
+            <ExpenseItem
+                id={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />)}
+    </ul>
+}
+
+export default ExpensesList;
